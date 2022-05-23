ada0d9e9b672a86a09c5d9b5630f5541db646d74
juansouza09 <juan.souza.git@gmail.com>
2022-05-23 20:20:25 -0300

    first commit

---
+++ app/src/main/java/com/example/luastation/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.example.luastation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cadastro_2_etapa_screen)
+    }
+}
